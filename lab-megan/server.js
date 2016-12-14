effdc16942b3c27cbf6cc367544d65dac0248418
Megan Reardon <meganr@gmail.com>
2016-12-14 09:29:45 -0800

    have cowsay get w error working

--- lab-megan/server.js
+++ lab-megan/server.js
@@ -37,38 +37,15 @@ const server = http.createServer(function(req, res) {
     console.log('The request querystring is:', req.url.query);
     var currentRequest = req.url.query;
 
-    // if (currentRequest === null) { // no
     if (req.url.query.text === undefined) {
       res.writeHead(400, {'Content-Type': 'text/plain' });
       res.write(cowsay.say({text: 'bad request'}));
-      //write head
-      //message
     } else {
       res.writeHead(200, {'Content-Type': 'text/plain' });
       res.write(cowsay.say(currentRequest));
     }
 
     res.end();
-
-    // if the body does not equal text=message
-      // return status code 400
-      // body including value from query
-      // and a body including the value returned from cowsay.say({ text: 'bad request' })
-    // else return
-      // status code of 200
-      // a response body that includes the value returned from cowsay.say({ text: <querystring text> })
-
-    //NEW TRY
-    //grab the body
-    // if it has == change to =
-
-    // if the query is text=message respond with
-      // status code of 200
-      // response body that includes the value returned from cowsay.say({ text: <querystring text> })
-    // if the text is not there respond with
-      // status code 400
-      // a body including the value returned from cowsay.say({ text: 'bad request' })
-
   }
 
   if(req.method === 'POST' && req.url.pathname === '/cowsay') {
@@ -77,8 +54,6 @@ const server = http.createServer(function(req, res) {
       if (err) console.error(err);
       console.log('POST request body is:', req.body);
     });
-    // res.end(); // this was here in class code, might need to put it back
-    // TODO ask ta why only get get cowsay is throwing errors
     res.end();
   }
 
